54e2eb3e0ab9f5c35330f523e7cdbd6c7d5eea15
Vitaly <onebeattrue@gmail.com>
2023-10-15 21:57:58 +0300

    State и рекомпозиция

--- app/src/main/java/edu/mirea/onebeattrue/vknewsclient/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/vknewsclient/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import edu.mirea.onebeattrue.vknewsclient.ui.theme.MainScreen
+import edu.mirea.onebeattrue.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            VkNewsClientTheme {
+                MainScreen()
+            }
         }
     }
 }
--- app/src/main/java/edu/mirea/onebeattrue/vknewsclient/ui/theme/MainScreen.kt
+++ app/src/main/java/edu/mirea/onebeattrue/vknewsclient/ui/theme/MainScreen.kt
@@ -3,11 +3,15 @@ package edu.mirea.onebeattrue.vknewsclient.ui.theme
 import android.annotation.SuppressLint
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -17,22 +21,32 @@ fun MainScreen() {
     Scaffold(
         bottomBar = {
             NavigationBar {
+                val selectedItemPosition = remember {
+                    mutableStateOf(0)
+                }
                 val items = listOf(
                     NavigationItem.Home,
                     NavigationItem.Favourite,
                     NavigationItem.Profile
                 )
 
-                items.forEach {
+                items.forEachIndexed { index, item ->
                     NavigationBarItem(
-                        selected = true,
-                        onClick = {},
+                        selected = index == selectedItemPosition.value,
+                        onClick = { selectedItemPosition.value = index },
                         icon = {
-                            Icon(it.icon, contentDescription = null)
+                            Icon(item.icon, contentDescription = null)
                         },
                         label = {
-                            Text(text = stringResource(id = it.titleResId))
-                        }
+                            Text(text = stringResource(id = item.titleResId))
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                            selectedTextColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                            indicatorColor = MaterialTheme.colorScheme.secondaryContainer,
+                            unselectedIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                            unselectedTextColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        )
                     )
                 }
             }
