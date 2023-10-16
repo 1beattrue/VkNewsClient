27ad89a362a8045dad0b88d24839a78e48d8661d
Vitaly <onebeattrue@gmail.com>
2023-10-16 17:20:01 +0300

    Рекомпозиция

--- app/src/main/java/edu/mirea/onebeattrue/vknewsclient/ui/theme/MainScreen.kt
+++ app/src/main/java/edu/mirea/onebeattrue/vknewsclient/ui/theme/MainScreen.kt
@@ -1,9 +1,9 @@
 package edu.mirea.onebeattrue.vknewsclient.ui.theme
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,19 +20,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
-    val snackbarHostState = SnackbarHostState()
+    val snackbarHostState = remember {
+        SnackbarHostState() // без этого при обращении к snackbarHostState снова вызовется функция MainScreen
+        // поэтому лучше все state'ы создавать таким образом
+    }
+
+    Log.d("MainScreen", snackbarHostState.toString())
+
     val scope = rememberCoroutineScope()
     var fabIsVisible by rememberSaveable {
         mutableStateOf(true)
