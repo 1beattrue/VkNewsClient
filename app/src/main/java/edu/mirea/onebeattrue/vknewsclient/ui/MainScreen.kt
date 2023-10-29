b393060ba9cc2bf4007ee9e8ae8df989302eb954
Vitaly <onebeattrue@gmail.com>
2023-10-29 21:55:18 +0300

    Исправил баги навигации Работа со стеком Запоминание state'а и его восстановление

--- app/src/main/java/edu/mirea/onebeattrue/vknewsclient/ui/MainScreen.kt
+++ app/src/main/java/edu/mirea/onebeattrue/vknewsclient/ui/MainScreen.kt
@@ -1,6 +1,7 @@
 package edu.mirea.onebeattrue.vknewsclient.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -8,12 +9,20 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import edu.mirea.onebeattrue.vknewsclient.navigation.AppNavGraph
+import edu.mirea.onebeattrue.vknewsclient.navigation.Screen
 import edu.mirea.onebeattrue.vknewsclient.presentation.MainViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -37,7 +46,15 @@ fun MainScreen(
                 items.forEach { item ->
                     NavigationBarItem(
                         selected = currentRoute == item.screen.route,
-                        onClick = { navHostController.navigate(item.screen.route) },
+                        onClick = {
+                            navHostController.navigate(item.screen.route) {
+                                launchSingleTop = true // позволяет хранить только один экземпляр экрана
+                                popUpTo(Screen.NewsFeed.route) {// рекомендация от Google при использовании BottomNavMenu
+                                    saveState = true // запоминаем state при удалении из бэкстэка
+                                }
+                                restoreState = true // восстанавливаем state при переходе к экрану
+                            }
+                        },
                         icon = {
                             Icon(item.icon, contentDescription = null)
                         },
@@ -59,8 +76,21 @@ fun MainScreen(
         AppNavGraph(
             navHostController = navHostController,
             homeScreenContent = { HomeScreen(viewModel = viewModel) },
-            favouriteScreenContent = { Text(text = "Favourite") },
-            profileScreenContent = { Text(text = "Profile") }
+            favouriteScreenContent = { TextCounter(name = "Favourite") },
+            profileScreenContent = { TextCounter(name = "Profile") }
         )
     }
+}
+
+@Composable
+private fun TextCounter(name: String) {
+    var count by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    Text(
+        modifier = Modifier.clickable { count++ },
+        text = "$name Count: $count",
+        color = Color.Black
+    )
 }
