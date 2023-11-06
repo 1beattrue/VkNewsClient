2b3ff27ef1ffcab82cee4af92c36b74e58e77440
Vitaly <onebeattrue@gmail.com>
2023-11-06 18:54:58 +0300

    Создание custom navigation type

--- app/src/main/java/edu/mirea/onebeattrue/vknewsclient/domain/FeedPost.kt
+++ app/src/main/java/edu/mirea/onebeattrue/vknewsclient/domain/FeedPost.kt
@@ -1,7 +1,13 @@
 package edu.mirea.onebeattrue.vknewsclient.domain
 
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.navigation.NavType
+import com.google.gson.Gson
 import edu.mirea.onebeattrue.vknewsclient.R
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class FeedPost(
     val id: Int = 0,
     val communityName: String = "Group name",
@@ -15,4 +21,21 @@ data class FeedPost(
         StatisticItem(type = StatisticType.COMMENTS, count = 42),
         StatisticItem(type = StatisticType.LIKES, count = 69)
     )
-)
+) : Parcelable {
+    companion object {
+        val NavigationType = object : NavType<FeedPost>(false) { // custom NavType
+            override fun get(bundle: Bundle, key: String): FeedPost? {
+                return bundle.getParcelable(key)
+            }
+
+            override fun parseValue(value: String): FeedPost {
+                return Gson().fromJson(value, FeedPost::class.java)
+            }
+
+            override fun put(bundle: Bundle, key: String, value: FeedPost) {
+                bundle.putParcelable(key, value)
+            }
+
+        }
+    }
+}
--- app/src/main/java/edu/mirea/onebeattrue/vknewsclient/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/edu/mirea/onebeattrue/vknewsclient/navigation/HomeScreenNavGraph.kt
@@ -2,11 +2,9 @@ package edu.mirea.onebeattrue.vknewsclient.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
-import com.google.gson.Gson
 import edu.mirea.onebeattrue.vknewsclient.domain.FeedPost
 
 fun NavGraphBuilder.homeScreenNavGraph(
@@ -25,12 +23,12 @@ fun NavGraphBuilder.homeScreenNavGraph(
             route = Screen.Comments.route,
             arguments = listOf(
                 navArgument(Screen.KEY_FEED_POST) {
-                    type = NavType.StringType
+                    type = FeedPost.NavigationType
                 }
             )
         ) {
-            val feedPostJson = it.arguments?.getString(Screen.KEY_FEED_POST) ?: ""
-            val feedPost = Gson().fromJson(feedPostJson, FeedPost::class.java)
+            val feedPost = it.arguments?.getParcelable<FeedPost>(Screen.KEY_FEED_POST)
+                ?: throw RuntimeException("Args is null")
             commentsScreenContent(feedPost)
         }
     }
